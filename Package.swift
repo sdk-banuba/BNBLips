89c54ea42f53eef284a1a986f7a3a96edda07f10
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-03-29 13:35:48 +0300

    1.11.1-70-g9084b7e01b

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.11.1-71-g826381c83"
+let bnbPackageVersion: Version = "1.11.1-70-g9084b7e01b"
 
 let package = Package(
     name: "BNBLips",
@@ -41,8 +41,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBLips",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1-71-g826381c83/BNBLips.zip",
-            checksum: "8a207ee4f47f4ed8f817cb9050b00a8553f0169f2fd5d4ec5004ae04f4c6f56f"
+            url: "None",
+            checksum: "9b243c01d61eaac515d33bbf67286c08768e6b16018cd2f5f00bc620c68bb38e"
         ),
         .target(
             name: "BNBLips_BNBSdkCore",
